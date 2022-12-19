742df7279170e385fd65cdeae8b91f14453f59fa
joaopaulofnmvc <103937437+joaopaulofnmvc@users.noreply.github.com>
2022-12-19 01:28:53 -0300

    refactor:ajustando menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ import { faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  const [menuActive, setMenuActive] = useState(true);
+  const [menuActive, setMenuActive] = useState(false);
 
   const menus = [
     {
